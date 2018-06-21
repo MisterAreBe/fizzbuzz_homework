55f0da133bd51296bfbe683013be89550ac3d76e
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 20:54:29 -0400

    test for array fail

--- test_tdd.rb
+++ test_tdd.rb
@@ -6,4 +6,10 @@ class TestFizzBuzz < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_array_exist
+        assert_equal(Array, fizzbuzz().class)
+    end
+
+    
 end
